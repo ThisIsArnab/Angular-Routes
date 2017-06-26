7a00e3eeb342debde1d3483a5fe551742735b48e
Arnab Das <das.arnab7@outlook.com>
2017-06-26 20:12:53 +0530

    Milestone 1 completed

--- Angular Application Test 05/src/app/app.module.ts
+++ Angular Application Test 05/src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeroListComponent } from './hero-list.component';
 import { CrisisListComponent} from './crisis-list.component';
 import { PageNotFoundComponent} from './not-found.component';
 
+const appRoutes: Routes = [
+    { path: 'crisis-center', component: CrisisListComponent },
+    { path: 'heroes', component: HeroListComponent },
+
+    { path: '', redirectTo: '/heroes', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
+];
+
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, RouterModule.forRoot(appRoutes), FormsModule ],
   declarations: [ AppComponent, HeroListComponent, CrisisListComponent, PageNotFoundComponent ],
   bootstrap:    [ AppComponent ]
 })
