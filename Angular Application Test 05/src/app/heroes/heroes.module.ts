fce2053ba6b647f0cb23a484577a41ece740a03b
Arnab Das <das.arnab7@outlook.com>
2017-06-27 08:54:56 +0530

    Initiating Milestone 3. Copied and modified required files from an old project

---
+++ Angular Application Test 05/src/app/heroes/heroes.module.ts
@@ -0,0 +1,14 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule} from '@angular/forms';
+
+import { HeroDetailComponent} from './hero-detail.component';
+import { HeroListComponent} from './hero-list.component';
+import { HeroService} from './hero.service';
+
+@NgModule({
+    imports: [CommonModule, FormsModule],
+    declarations: [HeroDetailComponent, HeroListComponent],
+    providers:[HeroService]
+})
+export class HeroModule {}
